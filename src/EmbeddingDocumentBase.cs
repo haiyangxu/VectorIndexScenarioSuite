0ed081f41538d8110cda52ed4c4a8b8814e7b16a
Haiyang <HaiyangXu@users.noreply.github.com>
2025-04-29 18:36:36 -0700

    add two label

--- src/EmbeddingDocumentBase.cs
+++ src/EmbeddingDocumentBase.cs
@@ -10,10 +10,19 @@ namespace VectorIndexScenarioSuite
          [JsonProperty(PropertyName = "embedding")]
         public float[] Embedding { get; }
 
+
+         [JsonProperty(PropertyName = "ranking")]
+        public uint Ranking { get; set; }
+
+        [JsonProperty(PropertyName = "pct")]
+        public uint Pct { get; set; }
+
         public EmbeddingDocumentBase(string id, float[] embedding)
         {
             this.Id = id;
             this.Embedding = embedding;
+            this.Ranking = uint.Parse(id);
+            this.Pct = uint.Parse(id) % 10000;
         }
     }
 }
